bdf7fadc7fc870abccf15952ec2dff17f1881f59
Salem Mohammed <93495898+salemmohmmed@users.noreply.github.com>
2021-12-22 08:56:48 +0300

    get and post

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const Todo = require("./Todo");
+app.use(express.json())
 
 // console.log(Todo)
 
@@ -9,11 +10,26 @@ app.get("/", (req, res) => {
   res.json("GET / IS WORKING");
 });
 app.get("/tasks", (req, res) => {
-    res.json("GET / IS WORKING");
+  Todo.find({}, (err, data) => {
+    if (err) {
+      console.log("ERROR", err);
+    } else {
+      res.json(data);
+    }
   });
+});
+
+app.post("/tasks", (req, res) => {
+
+  Todo.create(req.body, (err, newTask) => {
+    if (err) {
+      console.log("ERROR", err);
+    } else {
+      res.status(201).json( newTask);
+    }
+  });
+});
 
 app.listen(5000, () => {
   console.log("SERVER IS WORKING");
 });
-
-
