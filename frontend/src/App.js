fcb6161a5155072d677542024efcd7b5d24c8f80
Salem Mohammed <93495898+salemmohmmed@users.noreply.github.com>
2021-12-24 10:42:30 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
+import React,{useState} from "react";
+import "./App.css";
+import axios from "axios";
 
-export default  function App() {
-const getData =()=>{
-  axios
-  .get("http://localhost:5000/tasks")
-  .then((response)=>{
-    // console.log("RESPONSE",response)
-    console.log("DATA",response.data)
-  })
-  .catch((err)=>{
-    console.log("ERR:",err)
-  })
-}
 
+
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  const getData = () => {
+    //  من الباك ايند هذا يجيب داتا 
+    axios
+      .get("http://localhost:5000/tasks")
+      .then((response) => {
+        // console.log("RESPONSE",response)
+        console.log("DATA", response.data);
+        setTasks(response.data);
+      })
+      .catch((err) => {
+        console.log("ERR:", err);
+      });
+  };
+  
+  const mapOverTask = tasks.map((taskObj, i) => {
+    return <p>{taskObj.title} </p>;
+  });
 
   return (
     <div className="app">
       <p> App</p>
       <button onClick={getData}> GET tasck </button>
+      {mapOverTask}
     </div>
   );
 }
-
-
